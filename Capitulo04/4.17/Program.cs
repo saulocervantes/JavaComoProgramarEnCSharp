671c7a8b9f355e5887be603edd6569b28f1a473f
saulocervantes <saulo.cervantes@hotmail.com>
2016-11-26 01:01:41 -0600

    4.17 Programa que calcula el promedio de los kilometros recorridos por litros de gasolina.

---
+++ Capitulo04/4.17/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Los conductores se preocupan acerca del kilometraje de sus automóviles. Un conductor ha llevado el registro de
+varios reabastecimientos de gasolina, registrando los kilómetros conducidos y los litros usados en cada reabastecimiento.
+Desarrolle una aplicación en Java que reciba como entrada los kilómetros conducidos y los litros usados (ambos como
+enteros) por cada reabastecimiento.El programa debe calcular y mostrar los kilómetros por litro obtenidos en cada
+reabastecimiento, y debe imprimir el total de kilómetros por litro obtenidos en todos los reabastecimientos hasta este
+punto.Todos los cálculos del promedio deben producir resultados en números de punto flotante.Use la clase Scanner
+y la repetición controlada por centinela para obtener los datos del usuario.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rebastecimientos rebastecimientos = new Rebastecimientos();
+
+            rebastecimientos.pedirDatos();
+        }
+    }
+}
---
+++ Capitulo04/4.17/Rebastecimientos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._17
+{
+    class Rebastecimientos
+    {
+        public int Kilometros { get; set; }
+        public int Litros { get; set; }
+        public int TotalKm { get; set; }
+        public int TotalLt { get; set; }
+
+        public void pedirDatos()
+        {
+            string centinela = "continuar";
+            int contador = 0;
+
+            while ( centinela == "continuar")
+            {
+                contador++;
+                Console.WriteLine("Ingrese el número de kilometros en el rebastecimiento {0}", contador);
+                Kilometros = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese el número de litros en el rebastecimiento {0}", contador);
+                Litros = Convert.ToInt32(Console.ReadLine());
+
+                TotalKm += Kilometros;
+                TotalLt += Litros;
+
+                Console.WriteLine("Los kilometros por litro  en el rebastecimiento {1} son: {0}", KilometrosPorLitro(Kilometros, Litros), contador);
+                Console.WriteLine("Total de kilómetros por litro: {0}", KilometrosPorLitro(TotalKm, TotalLt));
+
+                centinela = Console.ReadLine();
+            }
+            
+        }
+
+        public float KilometrosPorLitro( int k, int l)
+        {
+            return (float)k / l;
+        }
+    }
+}
