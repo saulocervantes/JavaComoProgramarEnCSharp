4c05705076a07c06e51b657bbadca76bfd38fc65
saulocervantes <saulo.cervantes@hotmail.com>
2016-11-28 02:04:41 -0600

    4.22 Programa que dibuja una tabla con múltiplos de 10.

---
+++ Capitulo04/4.22/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ Escriba una aplicación en Java que utilice ciclos para imprimir la siguiente tabla de valores:
+N   10*N    100*N   1000*N
+1   10      100     1000
+2   20      200     2000
+3   30      300     3000
+4   40      400     4000
+5   50      500     5000
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("N   10*N    100*N   1000*N");
+
+            int i = 1;
+
+            while (i <= 10)
+            {
+                Console.WriteLine("{0,-4}{1,-8}{2,-8}{3,3}", i, 10 * i, 100 * i, 1000 * i);
+                ++i;
+            }
+        }
+    }
+}
