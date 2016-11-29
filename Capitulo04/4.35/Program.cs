f8750a6e03669724c47de8b0406809cab3b54069
saulocervantes <saulo.cervantes@hotmail.com>
2016-11-29 17:36:09 -0600

    4.35 Programa que recibe 3 números y determina si podrían representar los lados de un triángulo.

---
+++ Capitulo04/4.35/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Escriba una aplicación que lea tres valores distintos de cero introducidos por el usuario, y que determine e
+imprima si podrían representar los lados de un triángulo.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float cateto1 = float.MinValue;
+            float cateto2 = float.MinValue;
+            float hipotenusa = float.MinValue;
+            int i = 0;
+
+            while (i < 3)
+            {
+                ++i;
+                Console.WriteLine("Ingrese el tamaño del lado{0} del triángulo:", i);
+                float numero = float.Parse(Console.ReadLine());
+
+                if (i == 1)
+                    hipotenusa = numero;
+                else if (i == 2)
+                {
+                    cateto1 = Math.Min(hipotenusa, numero);
+                    hipotenusa = Math.Max(hipotenusa, numero);
+                }
+                else
+                {
+                    cateto2 = Math.Min(hipotenusa, numero);
+                    hipotenusa = Math.Max(hipotenusa, numero);
+                }
+
+            }
+            
+            Triangulo triangulo = new Triangulo(cateto1, cateto2, hipotenusa);
+            triangulo.VerificarTriangulo();
+
+
+        }
+    }
+}
---
+++ Capitulo04/4.35/Triangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._35
+{
+    class Triangulo
+    {
+        public float Cateto1 { get; set; }
+        public float Cateto2 { get; set; }
+        public float Hipotenusa { get; set; }
+
+        public Triangulo( float cat1, float cat2, float hip)
+        {
+            Cateto1 = cat1;
+            Cateto2 = cat2;
+            Hipotenusa = hip;
+        }
+
+        public void VerificarTriangulo()
+        {
+            int j = 0;
+
+            Console.WriteLine(Math.Pow(Hipotenusa, 2) == Math.Pow(Cateto1, 2) + Math.Pow(Cateto2, 2) ?
+                "SI es un triángulo" : "NO es un triángulo");
+        }
+    }
+}
