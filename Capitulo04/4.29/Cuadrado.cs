44874633b003ffa6f403a5e822ce91dfd6b18be5
saulocervantes <saulo.cervantes@hotmail.com>
2016-11-28 19:43:37 -0600

    4.29 Programa que dibuja un cuadrado según los lados que indique el usuario.

---
+++ Capitulo04/4.29/Cuadrado.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._29
+{
+    class Cuadrado
+    {
+        public int Lado { get; set; }
+
+        public void Dibujar()
+        {
+            int i = 1;
+            int j = 1;
+            int k = 1;
+
+            while (i <= Lado)
+            {
+                // Código para la primera y última línea
+                if (i == 1 || i == Lado)
+                {
+                    while (j <= Lado)
+                    {
+                        Console.Write( j == Lado ? "*\n" : "*");
+                        j++;
+                    }
+
+                    j = 1;
+                }
+                else // Código para las líneas del centro
+                {
+                    while (k <= Lado)
+                    {
+                        Console.Write( k == 1 ? "*" : "");
+                        Console.Write(k != 1 && k != Lado ? " " : "");
+                        Console.Write( k == Lado ? "*\n" : "");
+                        ++k;
+                    }
+
+                    k = 1;
+                }
+
+                i++;
+            }// Fin del While
+
+        }
+    }
+}
---
+++ Capitulo04/4.29/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ *Escriba una aplicación que pida al usuario que introduzca el tamaño del lado de un cuadrado y que muestre un
+cuadrado hueco de ese tamaño, compuesto de asteriscos. Su programa debe funcionar con cuadrados que tengan lados
+de todas las longitudes entre 1 y 20. 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuadrado cuadrado = new Cuadrado();
+
+            Console.WriteLine("Introduzca el tamaño del cuadrado:");
+            cuadrado.Lado = Convert.ToInt32( Console.ReadLine() );
+
+            cuadrado.Dibujar();
+        }
+    }
+}
