230123951095c1e56905bc30e4228b39446881b5
saulocervantes <saulo.cervantes@hotmail.com>
2016-11-30 00:52:52 -0600

    4.37 Programa que cifra y codifica enteros de 4 dígitos.

---
+++ Capitulo04/4.37/Entero.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._37
+{
+    class Entero
+    {
+        int digito1;
+        int digito2;
+        int digito3;
+        int digito4;
+        public int Numero { get; set; }
+
+        public void Cifrar()
+        {
+            //Método para cifrar el entero de 4 digitos
+            digito1 = (digito1 + 7) % 10;
+            digito2 = (digito2 + 7) % 10;
+            digito3 = (digito3 + 7) % 10;
+            digito4 = (digito4 + 7) % 10;
+
+            Numero = Convert.ToInt32(digito3 + "" + digito4 + "" + digito1 + "" + digito2);
+            Console.WriteLine("El número cifrado es {0:D4}", Numero);
+        }
+
+        public void Descifrar()
+        {
+            //Método para descifrar el entero de 4 dígitos
+            digito1 = (digito1 + 3) % 10;
+            digito2 = (digito2 + 3) % 10;
+            digito3 = (digito3 + 3) % 10;
+            digito4 = (digito4 + 3) % 10;
+
+            Numero = Convert.ToInt32(digito3 + "" + digito4 + "" + digito1 + "" + digito2);
+            Console.WriteLine("El número descifrado es {0:D4}", Numero);
+        }
+
+        public void SepararDigitos(int numero)
+        {
+            digito1 = numero / 1000 % 10;
+            digito2 = numero / 100 % 10;
+            digito3 = numero / 10 % 10;
+            digito4 = numero / 1 % 10;
+        }
+    }
+}
---
+++ Capitulo04/4.37/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Una compañía desea transmitir datos a través del teléfono, pero le preocupa que sus teléfonos puedan estar
+intervenidos. Le ha pedido a usted que escriba un programa que cifre sus datos, de manera que éstos puedan transmitirse
+con más seguridad. Todos los datos se transmiten como enteros de cuatro dígitos. Su aplicación debe leer un
+entero de cuatro dígitos introducido por el usuario y cifrarlo de la siguiente manera: reemplace cada dígito con el
+resultado de sumar 7 al dígito y obtener el residuo después de dividir el nuevo valor entre 10. Luego intercambie
+el primer dígito con el tercero, e intercambie el segundo dígito con el cuarto. Después imprima el entero cifrado.
+Escriba una aplicación separada que reciba como entrada un entero de cuatro dígitos cifrado, y que lo descifre para
+formar el número original.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Recibo entero
+            Console.WriteLine("Escriba un número entero de 4 dígitos:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            // Creo objeto
+            Entero entero = new Entero();
+
+            entero.SepararDigitos(numero);
+            entero.Cifrar();
+
+            Console.WriteLine("Ingrese un entero para decifrarlo");
+            numero = int.Parse(Console.ReadLine());
+
+            entero.SepararDigitos(numero);
+            entero.Descifrar();
+        }
+    }
+}
